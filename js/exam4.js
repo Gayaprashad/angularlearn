bcd7d39447707cb800f9e197c34ba670d7d674e5
unknown <trainee@PC339966.cts.com>
2016-08-02 17:54:21 +0530

    remain 10 min video2

--- js/exam4.js
+++ js/exam4.js
@@ -1,5 +1,7 @@
 var app4 = angular.module('app4', []);
 
 app4.controller('eventCtrl', function($scope){
-    $scope.disableButton=true;
+    $scope.dayTimeButton=true;
+
+    $scope.capitals=[{city:'chennai' , state:'tamilnadu'} ,{city:'lucknow',state:'Uttar pradesh'}];
 });
